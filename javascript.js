aea68c4e5cab50ff767a51f1954d16cdaa23ec44
Jason Keenan <jason.keenan@raavin.com>
2016-06-14 14:45:04 +1000

    fixed layout

--- javascript.js
+++ javascript.js
@@ -1,22 +1,24 @@
 jQuery(document).ready(function($) {
-	$.getJSON('https://api.twitch.tv/kraken/users/habathcx', function(data) {
-		console.log(data);
-        $("#freecodecamp").html(function() {
-          //$("#freecodecamp").html("")
-          //jQuery.each(data.query.search, function(index, item) {
-            $("#freecodecamp").html('<div class="caption">' +
-					'<h3>' + data.display_name + ' test</h3>' +
-					'<img src="' + data.logo + '" alt="">' +
-					'<p>' + data.bio.substring(0, 120) + '...test</p>' +
-					'<p><a href="#" role="button"><i class="fa fa-external-link fa-2x" aria-hidden="false"></i></a> </p>' +
-				'</div>');
-          	
-				
-          //});
-        });
-			/*optional stuff to do after success */
-			console.log(data.logo);
-			console.log(data.bio);
+	var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
+	users.forEach(function(item) {
+		$.getJSON('https://api.twitch.tv/kraken/users/freecodecamp', function(data) {
+			console.log(data);
+	        $("#freecodecamp").html(function() {
+	          //$("#freecodecamp").html("")
+	          //jQuery.each(data.query.search, function(index, item) {
+
+	            $("#freecodecamp").append(
+	            	
+		            	'<div class="thumbnail card card-up">'
+			+            	'<div class="caption">' +
+								'<h3>' + data.display_name + '</h3>' +
+								'<img src="' + data.logo + '" alt="">' +
+								'<p>' + data.bio.substring(0, 120) + '...</p>' +
+								'<p><a href="https://www.twitch.tv/' + data.name + '" role="button"><i class="fa fa-external-link fa-2x" aria-hidden="false"></i></a> </p>' +
+							'</div>' +
+					'</div>');
+	        	});
+	    	});
 	});
 
 	$.getJSON('https://api.twitch.tv/kraken/streams/freecodecamp', function(data) {
